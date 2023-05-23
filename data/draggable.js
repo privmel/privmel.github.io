e3169ca625e82923e751a1fed1d0420f4531e99c
mel dev <meltemp668@gmail.com>
2023-05-23 15:15:47 +0200

    big update wow

--- data/draggable.js
+++ data/draggable.js
@@ -1,11 +1,37 @@
+// making the windows draggable
+
 $(function(){
-  $('#drag').draggable();
+  $('#win').draggable();
 });
 
 $(function(){
-  $('#drag2').draggable();
+  $('#win2').draggable();
 });
 
 $(function(){
-  $('#drag3').draggable();
+  $('#win3').draggable();
 });
+
+$(function(){
+  $('#win4').draggable();
+});
+
+// show windows on click
+
+function openWin2() {
+  document.getElementById("win2").style.display = "";
+}
+
+function openWin4() {
+  document.getElementById("win4").style.display = "";
+}
+
+// close windows
+
+function close2() {
+  document.getElementById("win2").style.display = "none";
+}
+
+function close4() {
+  document.getElementById("win4").style.display = "none";
+}
